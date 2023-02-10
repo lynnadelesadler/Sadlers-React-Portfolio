3fd2a7207f71401a023c817dd99ec5a6d7b9e703
lynnadelesadler <lynnadelesadler@yahoo.com>
2023-02-10 16:01:30 -0500

    update resume with skills

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,25 @@
 import React from "react";
-
+import {
+  FaGitAlt,
+  FaGithub,
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaBootstrap,
+  FaNode,
+  FaReact,
+  FaNpm,
+  FaSass,
+} from "react-icons/fa";
+import { DiJqueryLogo, DiMongodb } from "react-icons/di";
+import {
+  SiMysql,
+  SiHeroku,
+  SiWebpack,
+  SiRedux,
+  SiReactrouter,
+  SiGraphql,
+} from "react-icons/si";
 import { Container, Row, Col } from "react-bootstrap";
 
 const styles = {
@@ -31,7 +51,86 @@ function Resume() {
     <p className="content" class="space" style={styles.background}>
       <h5>Skills</h5>
       <Container style={styles.container} className="container my-5">
- 
+      <Row>
+        <br></br>
+        <Col>
+        <h5>Technologies:</h5>
+          <br></br>
+          <h5>
+            • GitHub <FaGithub />
+          </h5>
+          <h5>
+            • Git <FaGitAlt />
+          </h5>
+          <h5>
+            • Heroku <SiHeroku />
+          </h5>
+          <h5>
+            • Webpack <SiWebpack />
+          </h5>
+          <br></br>
+        </Col>
+
+        <Col>
+          <h5>Front-end:</h5>
+          <br></br>
+          <h5>
+            • HTML
+            <FaHtml5 />
+          </h5>
+          <h5>
+            • CSS
+            <FaCss3Alt />
+          </h5>
+          <h5>
+            • JavaScript
+            <FaJsSquare />
+          </h5>
+          <h5>
+            • jQuery
+            <DiJqueryLogo />
+          </h5>
+          <h5>
+            • React
+            <FaReact /> <SiReactrouter /> <SiRedux />
+          </h5>
+          <h5>
+            • Bootstrap
+            <FaBootstrap />
+          </h5>
+          <h5>
+          • SASS
+            <FaSass/>
+          </h5>
+          <h5>
+          • Jest
+          </h5>
+        </Col>
+
+        <Col>
+          <h5>Back-end:</h5>
+          <br></br>
+          <h5>• APIs</h5>
+          <h5>
+            • Node
+            <FaNode /> <FaNpm />
+          </h5>
+          <h5>• Express
+          </h5>
+          <h5>
+            • MySQL, Sequelize <SiMysql />
+          </h5>
+          <h5>
+            • MongoDB, Mongoose
+            <DiMongodb />
+          </h5>
+          <h5>• REST</h5>
+          <h5>
+            • GraphQL
+            <SiGraphql />
+          </h5>
+        </Col>
+      </Row>
     </Container>
     </p>
   </section>
