b1d22696059088029c7870f9e95ab841e68276ff
lynnadelesadler <lynnadelesadler@yahoo.com>
2023-02-10 21:32:59 -0500

    update projects and add README back to repo

---
+++ README.md
@@ -0,0 +1,63 @@
+# Lynn Sadler Portfolio
+
+## Description
+
+This Portfolio is to provide information about myself and showcase my skills as a Full Stack web developer.
+* An About Me section will provide some personal information, fun facts and my professional career journey. 
+* A Contact section will provide an email, phone and git hub account when my repository projects can be viewed. 
+* A My Work section will link to all my full stack web development projects to showcase my skills. 
+* A Resume section will provide a link to download my resume. 
+
+## Access
+
+The URL of the GitHub repository ...
+
+https://github.com/lynnadelesadler/Sadlers-React-Portfolio
+
+The URL of the Deployed Github Page  ...
+
+https://lynnadelesadler.github.io/Sadlers-React-Portfolio/
+
+ 
+## Usage
+When viewing the deployed Lynn Sadler Portfolio website on a cell phone the website is responsive. The Portfolio project cards will stack horizontally to fit the page width.
+<!-- This text is Italics -->
+*Utilize the Lynn Sadler Portfolio webpage through it's nav bar.*
+
+<img src="./Images/navbar.png">
+
+<!-- Provide the navigation links in list format -->
+* Clicking on About Me in the navigation will link you to that section on webpage.
+
+
+*  Clicking on Portfolio in the navigation will link you to that section on webpage.
+
+ *Click on project card to launch the github repo or live webpage*
+
+
+*  Clicking on Contact in the navigation will link you to that section on webpage.
+
+
+*  Clicking on Resume in the navigation will link you to that section on webpage.
+
+*Click to open a PDF resume*
+
+
+ 
+## Support
+For help with this webpage please contact 
+|Name       | Email                     |
+|-----------|---------------------------|
+|Lynn Sadler| lynnadelesadler@yahoo.com |
+
+## Roadmap
+Check back to this page frequently as I continue to make updates to display new work I have released and enhancements on the coding and display of this Potrfolio!
+
+## Authors and acknowledgment
+
+Source-code credit to be given to Lynn Sadler. Acknowledgement to Uconn Full Stack Web Developer bootcamp for supporting me on my developer journey!
+
+
+
+## License
+N/A
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,7 @@ const projectList  = [
   {
     id: 0,
     title: "The Venue",
-    technologies: "html, handlebars, js, css",
+    technologies: "Spotify API, Node.js, MySQL, Sequelize,Express, Handlebars and SASS",
     image: "/Images/the-venue.jpg",
     description: "The Venue' is a music blog website created to discover, save, share, and socialize about music.",
     github: "https://github.com/ScottsC0de/the-venue",
@@ -16,7 +16,7 @@ const projectList  = [
   {
     id: 1,
     title: "Covid Tracker",
-    technologies: "html, js, css",
+    technologies: "COVID19 API, Reddit API, jQuery, Bootstrap and Moment.js",
     image: "/Images/world-map-banner.jpg",
     description: "Allows you to edit and post blogs. You can also leave comments on your own blog or the blogs of others.",
     github: "https://eaponte24.github.io/Covid-Tracker/",
@@ -25,7 +25,7 @@ const projectList  = [
   {
     id: 2,
     title: "Sadlers Blog Space",
-    technologies: "html, css, js, handlebars",
+    technologies: "Node.js, MySQL, Sequelize,Express and Handlebars",
     image: "/Images/blog-space.jpg",
     description: "Allows instructors to quickly create quizzes and print the quiz to a PDF to distribute to their students, as well as keep a copy for themselves with the answer key!",
     deployedLink: "https://sadlers-blog-space.herokuapp.com",
@@ -33,8 +33,8 @@ const projectList  = [
   },
   {
     id: 3,
-    title: "Sadlers NoSQL Employee Tracker",
-    technologies: "html, css, js",
+    title: "Sadlers NoSQL Social Network",
+    technologies: "MongoDB, Mongoose ODM, Node.js and Express",
     image: "/Images/noSQL-socialnetwork.jpg",
     description: "Sadlers NoSQL Social Network API is a social network web application where users can share their thoughts, react to friends thoughts, and create a friend list.",
     github: "https://github.com/lynnadelesadler/Sadlers-NoSQL-Social-Network-API",
@@ -43,7 +43,7 @@ const projectList  = [
   {
     id: 4,
     title: "Sadlers Note taker",
-    technologies: "html, css, js",
+    technologies: "Node.js and Express.js",
     image: "/Images/note-taker.jpg",
     description: "Sadler's Note Taker application can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.",
     github: "https://github.com/lynnadelesadler/Sadlers-Note-Taker",
@@ -52,20 +52,11 @@ const projectList  = [
   {
     id: 5,
     title: "Whats Up Weather",
-    technologies: "html, css, js",
+    technologies: "OpenWeather API and Moment.js",
     image: "/Images/weather-dashboard.jpg",
     description: "This App uses a third party API from Open Weather to request weather data from cities searched. Users will be able to search for a city and see the current weather in addition to the five day forecast. A users search history is saved in local storage and added to a search history list as a button which can render the search data for that city.",
     github: "https://github.com/lynnadelesadler/Whats-Up-Weather",
     deployedLink: "https://sadlers-note-taker.herokuapp.com/",
-  },
-  {
-    id: 6,
-    title: "Sadlers Employee Tracker",
-    technologies: "html, css, js",
-    image: "/Images/employee-tracker.jpg",
-    description: "Sadler's Employee Tracker application can be used by businesses to view and manage the departments, roles, and employees in their company stored in a database. Using a content management systems (CMS) allows the business to view and interact with the database.",
-    github: "https://github.com/lynnadelesadler/Sadlers-Employee-Tracker",
-    live: "",
   },
 ];
 
